1561a5d97b2e8279e64bdae6183dace9fa929d88
Javohir Narimonov <javohirnarimonov2009@gmail.com>
2026-01-26 17:49:43 +0500

    3dars

---
+++ app.js
@@ -0,0 +1,25 @@
+// Data types
+
+// Promitive data types - Oddiy, sodda malumot turlari
+// 1. String - yozuv
+let text = `string text`;
+console.log(text);
+
+// Template literals
+let userName = 'Dilshod';
+let userAge = 14;
+console.log("Foydalanuvchining ismi: " + userName + ", " + "yoshi " + userAge);
+console.log(`Foydalanuvchining ismi: ${userName}, yoshi ${userAge}`);
+
+
+
+// 2. Number -sonlar
+// 3. Boolean - true, false
+// 4. Null - bosh qiymat
+// 5. Underfined - qiymat berilmagan ozgaruvchi 
+// 6. BigInt - Katta sonlar
+
+// Nonprimitive data types - Murakkab malumot turlari
+// 8.  Objekt 
+// 9.  Array
+// 10. function
