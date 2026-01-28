cb01abdc7bb775144b0b84f7fe1d766e7ee03a5b
Javohir Narimonov <javohirnarimonov2009@gmail.com>
2026-01-28 17:23:09 +0500

    dilshod

--- app.js
+++ app.js
@@ -2,24 +2,38 @@
 
 // Promitive data types - Oddiy, sodda malumot turlari
 // 1. String - yozuv
-let text = `string text`;
-console.log(text);
+// let text = `string text`;
+// console.log(text);
 
 // Template literals
-let userName = 'Dilshod';
-let userAge = 14;
-console.log("Foydalanuvchining ismi: " + userName + ", " + "yoshi " + userAge);
-console.log(`Foydalanuvchining ismi: ${userName}, yoshi ${userAge}`);
+// let userName = 'Dilshod';
+// let userAge = 14;
+// console.log("Foydalanuvchining ismi: " + userName + ", " + "yoshi " + userAge);
+// console.log(`Foydalanuvchining ismi: ${userName}, yoshi ${userAge}`);
 
 
 
 // 2. Number -sonlar
+let number = 1_000_000
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MIN_SAFE_INTEGER);
+
+
 // 3. Boolean - true, false
+let bool = false;
 // 4. Null - bosh qiymat
+let empty = null
+console.log(null == 0); //false
 // 5. Underfined - qiymat berilmagan ozgaruvchi 
+let userName;
+console.log(userName); // underfined
 // 6. BigInt - Katta sonlar
 
+let example_one = 9007199254740991n + 1n
+let example_two = -9007199254740991n + 2n
+
+// Symbol - shifrash
 // Nonprimitive data types - Murakkab malumot turlari
-// 8.  Objekt 
+// 8.  Objekt
 // 9.  Array
 // 10. function
